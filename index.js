16f8939885c5bfc2d2b9525d61163d5b15440854
pawulista <jeankarekezi1010@gmail.com>
2026-09-16 19:13:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello word")
+console.log("Jean Karekezi");
+console.log("i am learning Node.js");
+console.log("day is completed");
+git remote add origin https://github.com/pawulista/day-one.git
+
+
+
+
