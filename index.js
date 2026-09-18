0e6f52160f0d4492595b2ae7a63dcd7ed5754724
pawulista <jeankarekezi1010@gmail.com>
2026-09-18 15:48:57 +0200

    i made changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("hello word")
 console.log("Jean Karekezi");
 console.log("i am learning Node.js");
 console.log("day is completed");
-git remote add origin https://github.com/pawulista/day-one.git
+
 
 
 
